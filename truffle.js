4c0a97d158a7cf5ef13134972716c0c47260f669
bezalel <bezalel.dev@gmail.com>
2017-11-23 18:57:14 +0900

    truffle setup

---
+++ truffle.js
@@ -0,0 +1,29 @@
+module.exports = {
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*"
+        },
+        ropsten: {
+            network_id: 3, // ropsten testnet
+            host: "localhost",
+            port: 8546,
+            gas: 4712384,
+            gasPrice: 90000000000000
+        },
+        rinkeby: {
+            network_id: 4, // rinkeby testnet
+            host: "localhost",
+            port: 8547,
+            gas: 4500000
+        },
+        live: {
+            network_id: 1,
+            host: "localhost",
+            port: 8548,
+            gas: 4712384
+        }
+    },
+    migrations_directory: './migrations'
+}
